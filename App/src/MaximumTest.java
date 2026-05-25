8a1878ecc6ba0d4a9bbd24de9756422bfc5f3ff5
sandeep <sandeepmishra898k@gmail.com>
2026-05-25 17:35:04 +0530

    Refactor 2 — Generic Class

--- App/src/MaximumTest.java
+++ App/src/MaximumTest.java
@@ -1,4 +1,16 @@
-public class MaximumTest {
+public class MaximumTest<T extends Comparable<T>> {
+
+    T x, y, z;
+
+    public MaximumTest(T x, T y, T z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public T testMaximum() {
+        return testMaximum(x, y, z);
+    }
 
     public static <T extends Comparable<T>> T testMaximum(T x, T y, T z) {
 
@@ -17,10 +29,19 @@ public class MaximumTest {
 
     public static void main(String[] args) {
 
-        System.out.println(testMaximum(30, 20, 10));
+        MaximumTest<Integer> intTest =
+                new MaximumTest<>(10, 20, 30);
 
-        System.out.println(testMaximum(10.5f, 40.5f, 20.5f));
+        System.out.println(intTest.testMaximum());
 
-        System.out.println(testMaximum("Apple", "Peach", "Banana"));
+        MaximumTest<Float> floatTest =
+                new MaximumTest<>(10.5f, 40.5f, 20.5f);
+
+        System.out.println(floatTest.testMaximum());
+
+        MaximumTest<String> stringTest =
+                new MaximumTest<>("Apple", "Peach", "Banana");
+
+        System.out.println(stringTest.testMaximum());
     }
 }
